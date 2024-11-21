fee13cb56a49bb6646d0b70c29be0aafd09d5b62
ROJAS VILLALOBOS <josue.rojas9@ulatina.net>
2024-11-21 17:10:01 -0600

    Josue Rojas, modificacion de entidad peiodo

---
+++ ProyectoGestorEscolar/Logica/Entidades/Periodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoGestorEscolar.Logica.Entidades
+{
+    public class Periodo
+    {
+        public int ano {  get; set; }
+        public String ciclo { get; set; }
+    }
+}

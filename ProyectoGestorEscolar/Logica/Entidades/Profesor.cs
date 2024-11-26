24fb7171aa57801fb2c7db201dbb954beb1e31a7
David Esteban Monge López <david.monge2cr@hotmail.com>
2024-11-25 22:53:03 -0600

    Cambios Profesor David Monge

--- ProyectoGestorEscolar/Logica/Entidades/Profesor.cs
+++ ProyectoGestorEscolar/Logica/Entidades/Profesor.cs
@@ -10,17 +10,12 @@ namespace ProyectoGestorEscolar.Logica.Entidades
     {
         public String NombreUsuario { get; set; }
         public String PSWD { get; set; }
-        public int Curso { get; set; }
         public String Nombre { get; set; }
         public String Apellidos { get; set; }
-        public String Materias { get; set; }
-        public String Direccion { get; set; }
-        public String Cedula { get; set; }
-        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaInicio { get; set; }
         public String Telefono { get; set; }
-        public String Calificacion { get; set; }
+        public String Cedula { get; set; }
         public String Genero { get; set; }
         public String Correo { get; set; }
-        public DateTime FechaInicio { get; set; }
     }
 }
